3af563267d28209b931caf1abcc9740cc4eb7b1f
6410615030 <6410615030@student.tu.ac.th>
2024-02-11 23:40:17 +0700

    Initial Commit

---
+++ Puzzle15Game/Puzzle15GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Puzzle15GameApp.swift
+//  Puzzle15Game
+//
+//  Created by Babypowder on 11/2/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct Puzzle15GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
